cd09d4da91fdf90969c1667b61bb26eb34ced317
Guilherme Bafica <gb.engcomp@hotmail.com>
2024-03-25 19:00:27 -0400

    Implements layout config

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,7 @@ import { View, Text } from "react-native";
 
 export default function Home() {
   return (
-    <View className="bg-slate-900 flex-1">
+    <View className="flex-1 pt-8">
       <Text className="text-white">Hello World!</Text>
     </View>
   );
