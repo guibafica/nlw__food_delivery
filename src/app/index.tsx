e68293da6d907adb6a0c9beac96ba9d6a9297666
Guilherme Bafica <gb.engcomp@hotmail.com>
2024-03-25 17:17:17 -0400

    Config nativewind

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,8 +2,8 @@ import { View, Text } from "react-native";
 
 export default function Home() {
   return (
-    <View>
-      <Text>Hello World!</Text>
+    <View className="bg-slate-900 flex-1">
+      <Text className="text-white">Hello World!</Text>
     </View>
   );
 }
